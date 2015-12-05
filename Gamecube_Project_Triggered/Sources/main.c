419583613540ddc704939445d4fbb20422d7827c
Jordan Huffaker <jordhuff@gmail.com>
2015-12-05 12:41:18 -0500

    Changed scheme.h to not have definitions of each scheme and moved them to scheme.c.

--- Gamecube_Project_Triggered/Sources/main.c
+++ Gamecube_Project_Triggered/Sources/main.c
@@ -2,21 +2,32 @@
 #include "microcontroller.h"
 #include "control.h"
 
+#define CYCLESFOR40MICROWAIT 10
+
+static void wait40MicroSeconds(void);
+
 void main(void)
 {
 	initializations();
 	
 	for(;;)
 	{
-			
+		if (Transaction_Complete)
+		{
+			// Clear Transaction_Complete flag
+			Transaction_Complete = 0;
 
+			// Wait 40 Micro Seconds
+			wait40MicroSeconds();
+
+			// Get the next input from the control and send it out
+			//setOutputFrame(GetNextInput(getInputFrame()));
+		}
 	}
-}  
+}
 
-
-
-
-
-
-
-
+static void wait40MicroSeconds()
+{
+	int i;
+	for (i = 0; i < CYCLESFOR40MICROWAIT; i++) {}
+}
--- Gamecube_Project_Triggered/Sources/scheme.h
+++ Gamecube_Project_Triggered/Sources/scheme.h
@@ -16,9 +16,9 @@ struct Scheme_st
 	unsigned char ZButton;
 };
 
-extern Scheme Scheme1 = { MACRO1, MACRO2, MACRO3, MACRO4, MACRO5, MACRO6, MACRO7 };
-extern Scheme Scheme2 = { MACRO8, MACRO9, MACRO10, MACRO11, MACRO12, MACRO13, MACRO14 };
-extern Scheme Scheme3 = { MACRO15, MACRO16, MACRO17, MACRO18, MACRO19, MACRO20, MACRO21 };
-extern Scheme Scheme4 = { MACRO22, MACRO23, MACRO24, MACRO25, MACRO26, MACRO27, MACRO28 };
+extern Scheme Scheme1;
+extern Scheme Scheme2;
+extern Scheme Scheme3;
+extern Scheme Scheme4;
 
 #endif /* SCHEME_INCLUDED */
