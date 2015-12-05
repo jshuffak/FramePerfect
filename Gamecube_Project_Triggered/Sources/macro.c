e5a208918becc27d7c38a6bf69cf6227a42a30c7
Jordan Huffaker <jhuffak@purdue.edu>
2015-12-05 12:27:10 -0500

    Update macro.c

--- Gamecube_Project_Triggered/Sources/macro.c
+++ Gamecube_Project_Triggered/Sources/macro.c
@@ -1,96 +1,33 @@
 #include "macro.h"
-//The size of each macro
-#define MAC_1 1
-#define MAC_2 1
-#define MAC_3 1
-#define MAC_4 1
-#define MAC_5 1
-#define MAC_6 1
-#define MAC_7 1
-#define MAC_8 1
-#define MAC_9 1
-#define MAC_10 1
-#define MAC_11 1
-#define MAC_12 1
-#define MAC_13 1
-#define MAC_14 1
-#define MAC_15 1
-#define MAC_16 1
-#define MAC_17 1
-#define MAC_18 1
-#define MAC_19 1
-#define MAC_20 1
-#define MAC_21 1
-#define MAC_22 1
-#define MAC_23 1
-#define MAC_24 1
-#define MAC_25 1
-#define MAC_26 1
-#define MAC_27 1
-#define MAC_28 1
-#define MACS_1 0
-//The Start of each Macro
-#define MACS_2 MAC_1
-#define MACS_3 MAC_1 + MAC_2
-#define MACS_4 MAC_1 + MAC_2 + MAC_3
-#define MACS_5 MAC_1 + MAC_2 + MAC_3 + MAC_4
-#define MACS_6 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5
-#define MACS_7 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6
-#define MACS_8 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7
-#define MACS_9 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8
-#define MACS_10 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9
-#define MACS_11 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10
-#define MACS_12 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11
-#define MACS_13 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12
-#define MACS_14 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13
-#define MACS_15 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13 + MAC_14
-#define MACS_16 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13 + MAC_14 + MAC_15
-#define MACS_17 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13 + MAC_14 + MAC_15 + MAC_16
-#define MACS_18 MACS_17 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13 + MAC_14 + MAC_15 + MAC_16 + MAC_17
-#define MACS_19 MACS_17 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13 + MAC_14 + MAC_15 + MAC_16 + MAC_17 + MAC_18
-#define MACS_20 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13 + MAC_14 + MAC_15 + MAC_16 + MAC_17 + MAC_18 + MAC_19
-#define MACS_21 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13 + MAC_14 + MAC_15 + MAC_16 + MAC_17 + MAC_18 + MAC_19 + MAC_20
-#define MACS_22 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13 + MAC_14 + MAC_15 + MAC_16 + MAC_17 + MAC_18 + MAC_19 + MAC_20 + MAC_21
-#define MACS_23 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13 + MAC_14 + MAC_15 + MAC_16 + MAC_17 + MAC_18 + MAC_19 + MAC_20 + MAC_21 + MAC_22
-#define MACS_24 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13 + MAC_14 + MAC_15 + MAC_16 + MAC_17 + MAC_18 + MAC_19 + MAC_20 + MAC_21 + MAC_22 + MAC_23
-#define MACS_25 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13 + MAC_14 + MAC_15 + MAC_16 + MAC_17 + MAC_18 + MAC_19 + MAC_20 + MAC_21 + MAC_22 + MAC_23 + MAC_24
-#define MACS_26 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13 + MAC_14 + MAC_15 + MAC_16 + MAC_17 + MAC_18 + MAC_19 + MAC_20 + MAC_21 + MAC_22 + MAC_23 + MAC_24 + MAC_25
-#define MACS_27 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13 + MAC_14 + MAC_15 + MAC_16 + MAC_17 + MAC_18 + MAC_19 + MAC_20 + MAC_21 + MAC_22 + MAC_23 + MAC_24 + MAC_25 + MAC_26
-#define MACS_28 MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13 + MAC_14 + MAC_15 + MAC_16 + MAC_17 + MAC_18 + MAC_19 + MAC_20 + MAC_21 + MAC_22 + MAC_23 + MAC_24 + MAC_25 + MAC_26 + MAC_27
-
-
 
 //Definitions
 Input Macro_array[MAC_1 + MAC_2 + MAC_3 + MAC_4 + MAC_5 + MAC_6 + MAC_7 + MAC_8 + MAC_9 + MAC_10 + MAC_11 + MAC_12 + MAC_13 + MAC_14 + MAC_15 + MAC_16 + MAC_17 + MAC_18 + MAC_19 + MAC_20 + MAC_21 + MAC_22 + MAC_23 + MAC_24 + MAC_25 + MAC_26 + MAC_27 + MAC_28];
 
-Macro_array[0] = { .Garbage = 0, .Start = 0, .Y = 1, .X = 0, .B = 0, .A = 0, .Garbage1 = 0, .L = 0, .R = 0, .Z = 0, .DUp = 0, .DDown = 0, .DRight = 0, .DLeft = 0, .JoyX = 0, .JoyY = 0, .CStickX = 0, .CStickY = 0, .LeftButt = 0, .RightButt = 0};
-
-
 GetMacroInput(MacroFrame frame, Input* output)
 {
 
-  if(!(Macro_array[frame.Macro - 1][frame.Frame].Garbage == 3))
+  if(!(Macro_array[frame.Frame].Garbage == 3))
   {
-    output->Garbage = Macro_array[frame.Macro - 1][frame.Frame].Garbage;
-    output->Start = Macro_array[frame.Macro - 1][frame.Frame].Start;
-    output->Y = Macro_array[frame.Macro - 1][frame.Frame].Y;
-    output->X = Macro_array[frame.Macro - 1][frame.Frame].X;
-    output->B = Macro_array[frame.Macro - 1][frame.Frame].B;
-    output->A = Macro_array[frame.Macro - 1][frame.Frame].A;
-    output->Garbage1 = Macro_array[frame.Macro - 1][frame.Frame].Garbage1;
-    output->L = Macro_array[frame.Macro - 1][frame.Frame].L;
-    output->R = Macro_array[frame.Macro - 1][frame.Frame].R;
-    output->Z = Macro_array[frame.Macro - 1][frame.Frame].Z;
-    output->DUp = Macro_array[frame.Macro - 1][frame.Frame].DUp;
-    output->DDown = Macro_array[frame.Macro - 1][frame.Frame].DDown;
-    output->DRight = Macro_array[frame.Macro - 1][frame.Frame].DRight;
-    output->DLeft = Macro_array[frame.Macro - 1][frame.Frame].DLeft;
-    output->JoyX = Macro_array[frame.Macro - 1][frame.Frame].JoyX;
-    output->JoyY = Macro_array[frame.Macro - 1][frame.Frame].JoyY;
-    output->CStickX = Macro_array[frame.Macro - 1][frame.Frame].CStickX;
-    output->CStickY = Macro_array[frame.Macro - 1][frame.Frame].CStickY;
-    output->LeftButt = Macro_array[frame.Macro - 1][frame.Frame].LeftButt;
-    output->RightButt = Macro_array[frame.Macro - 1][frame.Frame].RightButt;
+    output->Garbage = Macro_array[frame.Frame + frame.Macro].Garbage;
+    output->Start = Macro_array[frame.Frame + frame.Macro].Start;
+    output->Y = Macro_array[frame.Frame + frame.Macro].Y;
+    output->X = Macro_array[frame.Frame + frame.Macro].X;
+    output->B = Macro_array[frame.Frame + frame.Macro].B;
+    output->A = Macro_array[frame.Frame + frame.Macro].A;
+    output->Garbage1 = Macro_array[frame.Frame + frame.Macro].Garbage1;
+    output->L = Macro_array[frame.Frame + frame.Macro].L;
+    output->R = Macro_array[frame.Frame + frame.Macro].R;
+    output->Z = Macro_array[frame.Frame + frame.Macro].Z;
+    output->DUp = Macro_array[frame.Frame + frame.Macro].DUp;
+    output->DDown = Macro_array[frame.Frame + frame.Macro].DDown;
+    output->DRight = Macro_array[frame.Frame + frame.Macro].DRight;
+    output->DLeft = Macro_array[frame.Frame + frame.Macro].DLeft;
+    output->JoyX = Macro_array[frame.Frame + frame.Macro].JoyX;
+    output->JoyY = Macro_array[frame.Frame + frame.Macro].JoyY;
+    output->CStickX = Macro_array[frame.Frame + frame.Macro].CStickX;
+    output->CStickY = Macro_array[frame.Frame + frame.Macro].CStickY;
+    output->LeftButt = Macro_array[frame.Frame + frame.Macro].LeftButt;
+    output->RightButt = Macro_array[frame.Frame + frame.Macro].RightButt;
   }
   else
   {
@@ -98,6 +35,7 @@ GetMacroInput(MacroFrame frame, Input* output)
   }
   return 0;
 }
+
 
 
 
